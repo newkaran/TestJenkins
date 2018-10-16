e06ef4b969896b52d97382a8b194f6acffd42fce
GSDTeam <GSDTeam@EBHVR15-03-01.hexa.training.local>
2018-10-16 16:22:15 +0530

    FirstCommit

---
+++ testProject/src/com/SampleTest.java
@@ -0,0 +1,10 @@
+package com;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("You are watching me through Jenkins Build...");
+	}
+
+}
